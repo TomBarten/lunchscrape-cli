182893aea89005914427f52940fd3d786f49bc75
Tom Barten <17269387+TomBarten@users.noreply.github.com>
2023-10-16 14:30:47 +0200

    Added vscode go settings to repo + inserts spaces now instead of tabs

--- lunchscrape.go
+++ lunchscrape.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+    "fmt"
+    "strconv"
+    "strings"
 
-	"github.com/gocolly/colly"
+    "github.com/gocolly/colly"
 )
 
 type currency struct {
-	CurrencySymbol string  `json:"currency-symbol"`
-	Value          float64 `json:"value"`
+    CurrencySymbol string  `json:"currency-symbol"`
+    Value          float64 `json:"value"`
 }
 
 type item struct {
-	Name        string `json:"name"`
-	Price       currency
-	Description string `json:"description"`
-	ImgUrl      string `json:"imgurl"`
+    Name        string `json:"name"`
+    Price       currency
+    Description string `json:"description"`
+    ImgUrl      string `json:"imgurl"`
 }
 
 func main() {
 
-	baseDomain := "cafetariabienvenue.12waiter.eu"
+    baseDomain := "cafetariabienvenue.12waiter.eu"
 
-	collector := colly.NewCollector(
-		colly.AllowedDomains(baseDomain),
-	)
+    collector := colly.NewCollector(
+        colly.AllowedDomains(baseDomain),
+    )
 
-	collector.OnHTML("a[class=product-item]", collectProductItems)
+    collector.OnHTML("a[class=product-item]", collectProductItems)
 
-	collector.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visiting", request.URL)
-	})
+    collector.OnRequest(func(request *colly.Request) {
+        fmt.Println("Visiting", request.URL)
+    })
 
-	collector.Visit(fmt.Sprintf("https://%s/c/topped-friet", baseDomain))
+    collector.Visit(fmt.Sprintf("https://%s/c/topped-friet", baseDomain))
 }
 
 func collectProductItems(element *colly.HTMLElement) {
-	rawItemPrice := element.ChildText("div.product-item-body div.product-item-offer span.product-item-price")
+    rawItemPrice := element.ChildText("div.product-item-body div.product-item-offer span.product-item-price")
 
-	itemPriceParts := strings.Fields(rawItemPrice)
+    itemPriceParts := strings.Fields(rawItemPrice)
 
-	if len(itemPriceParts) != 2 {
-		fmt.Printf("Invalid item price format: %s", rawItemPrice)
-		return
-	}
+    if len(itemPriceParts) != 2 {
+        fmt.Printf("Invalid item price format: %s", rawItemPrice)
+        return
+    }
 
-	currencySymbol := itemPriceParts[0]
+    currencySymbol := itemPriceParts[0]
 
-	itemPriceStr := strings.ReplaceAll(itemPriceParts[1], ",", ".")
+    itemPriceStr := strings.ReplaceAll(itemPriceParts[1], ",", ".")
 
-	itemPrice, conversionError := strconv.ParseFloat(itemPriceStr, 64)
+    itemPrice, conversionError := strconv.ParseFloat(itemPriceStr, 64)
 
-	if conversionError != nil {
-		fmt.Println(conversionError)
-		return
-	}
+    if conversionError != nil {
+        fmt.Println(conversionError)
+        return
+    }
 
-	item := item{
-		Name:        element.ChildText("div.product-item-body div[class=product-item-title]"),
-		Description: element.ChildText("div.product-item-body div[class=product-item-description]"),
-		Price: currency{
-			CurrencySymbol: currencySymbol,
-			Value:          itemPrice,
-		},
-	}
-	fmt.Println(item)
+    item := item{
+        Name:        element.ChildText("div.product-item-body div[class=product-item-title]"),
+        Description: element.ChildText("div.product-item-body div[class=product-item-description]"),
+        Price: currency{
+            CurrencySymbol: currencySymbol,
+            Value:          itemPrice,
+        },
+    }
+    fmt.Println(item)
 }
